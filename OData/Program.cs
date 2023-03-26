e906423b2fd74fd34dbac8b0a1f2465732da4df4
Petr Unzeitig <unzeitip@gmail.com>
2023-03-26 09:55:34 +0200

    fix Roles composite key edm model bug

--- OData/Program.cs
+++ OData/Program.cs
@@ -1,22 +1,17 @@
-using Shared.Models;
 using Microsoft.AspNetCore.OData;
-using Microsoft.OData.ModelBuilder;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OData.Edm;
+using Microsoft.OData.ModelBuilder;
+using Shared.Models;
 
 var builder = WebApplication.CreateBuilder(args);
-
-var modelBuilder = new ODataConventionModelBuilder();
-modelBuilder.EntitySet<Actor>("actors");
-modelBuilder.EntitySet<Movie>("movies");
-modelBuilder.EntitySet<Director>("directors");
-modelBuilder.EntitySet<Role>("roles");
 
 builder.Services.AddDbContext<ImdbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("imdb")));
 
 builder.Services.AddControllers().AddOData(
     options => options.EnableQueryFeatures(null)
-    .AddRouteComponents("api", modelBuilder.GetEdmModel()));
+    .AddRouteComponents("api", GetEdmModel())); ;
 
 var app = builder.Build();
 
@@ -25,3 +20,16 @@ app.UseRouting();
 app.UseEndpoints(endpoints => endpoints.MapControllers());
 
 app.Run();
+
+static IEdmModel GetEdmModel()
+{
+    var modelBuilder = new ODataConventionModelBuilder();
+    modelBuilder.EntitySet<Actor>("actors");
+    modelBuilder.EntitySet<Movie>("movies");
+    modelBuilder.EntitySet<Director>("directors");
+
+    modelBuilder.EntityType<Role>().HasKey(r => new { r.ActorId, r.MovieId, r.RoleName });
+    modelBuilder.EntitySet<Role>("roles");
+
+    return modelBuilder.GetEdmModel();
+}
